fd36ff9470565e69416a601bcee46fd64334d4d9
vaishnavihole <vaishnavihole1@gmail.com>
2023-08-30 14:07:42 +0530

    add js logic to input-box

---
+++ script.js
@@ -0,0 +1,37 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", () =>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+// function Test()
+// {
+//     element = document.querySelector(".notes-container");
+//     element.innerHTML =  `
+//         <p contenteditable="true" class="input-box">
+//             <img src="images/delete.png">
+//         </p>`
+// }
