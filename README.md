e2cd20c0d50cf1cab5196edf4bed937709d269fa
vaishnavihole <vaishnavihole1@gmail.com>
2023-09-02 11:24:40 +0530

    readme file added

---
+++ README.md
@@ -0,0 +1,5 @@
+# NOTES APP
+
+![Notes App](./screenshot/HomePage.png)
+
+This is a simple Notes App. Creted using `querySelector` and `querySelectorAll` functions of JavaScript.
--- script.js
+++ script.js
@@ -2,14 +2,7 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
-
-
-function upadteStorage() {
-   localStorage.setItem("notes", notesContainer.innerHTML)
-}
-
-
-createBtn.addEventListener("click", () =>{
+createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
@@ -18,19 +11,12 @@ createBtn.addEventListener("click", () =>{
     notesContainer.appendChild(inputBox).appendChild(img);
 })
 
-notesContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "IMG"){
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
-        upadteStorage();
+
     }
-    else if (e.target.tagName === "p"){
-       notes = document.querySelectorAll(".input-box"); 
-       notes.forEach(nt => {
-        nt.onkeyup = function() {
-         upadteStorage();
-        }
-       })
-    }
+
 })
 
 
